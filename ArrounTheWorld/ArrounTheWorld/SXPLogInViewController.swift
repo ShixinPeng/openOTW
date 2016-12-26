78352fd32f30c5281d38a3bb36ed0f6959220df0
pengshixin <shixinpeng0429@163.com>
2016-12-26 13:51:19 +0800

    creat project

---
+++ ArrounTheWorld/ArrounTheWorld/SXPLogInViewController.swift
@@ -0,0 +1,35 @@
+//
+//  SXPLogInViewController.swift
+//  ArrounTheWorld
+//
+//  Created by shixinPeng on 16/12/26.
+//  Copyright © 2016年 pengshixin. All rights reserved.
+//
+
+import UIKit
+
+class SXPLogInViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = UIColor.yellow;
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ ArrounTheWorld/ArrounTheWorld/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  ArrounTheWorld
+//
+//  Created by shixinPeng on 16/12/26.
+//  Copyright © 2016年 pengshixin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = UIColor.red;
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
